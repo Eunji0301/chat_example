b0ca91317b077ce54406c96996a3297cd67822e0
Eunji0301 <forstudy.eunji@gmail.com>
2025-02-03 16:36:22 +0900

    json 형태로 데이터 전달, record

--- src/main/java/org/koreait/example/chat_example/ChatController.java
+++ src/main/java/org/koreait/example/chat_example/ChatController.java
@@ -1,20 +1,56 @@
 package org.koreait.example.chat_example;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/chat")
 public class ChatController {
 
-    chatMessage message = new chatMessage("김철수", "안녕");
+    private List<chatMessage> chatMessages = new ArrayList<chatMessage>();
+
+//    @AllArgsConstructor
+//    @Getter
+//    public static class writeChatMessageRequest {
+//        private final String authorName;
+//        private final String content;
+//    }
+
+    public record writeChatMessageRequest(String authorName, String content) {
+    }
+
+    public record writeChatMessageResponse(long id) {
+
+    }
 
     @PostMapping("/writeMessage")
     @ResponseBody
-    public RsData<chatMessage> writeMessage() {
-        return new RsData<>("S-1", "메세지가 작성됨", message);
+    public RsData<writeChatMessageResponse> writeMessage(@RequestBody writeChatMessageRequest req) {
+        chatMessage message = new chatMessage(req.authorName(), req.content());
+        chatMessages.add(message);
+        return new RsData<>(
+                "S-1",
+                "메세지가 작성됨",
+                new writeChatMessageResponse(message.getId())
+        );
+    }
+
+
+    public record messagesResponse(List<chatMessage> messages, long count) {
+
+    }
+
+    @GetMapping("/messages")
+    @ResponseBody
+    public RsData<messagesResponse> messages() {
+        return new RsData<>(
+                "S-1",
+                "메세지 리스트",
+                new messagesResponse(chatMessages, chatMessages.size())
+        );
     }
 
 }
--- src/main/java/org/koreait/example/chat_example/chatMessage.java
+++ src/main/java/org/koreait/example/chat_example/chatMessage.java
@@ -1,5 +1,6 @@
 package org.koreait.example.chat_example;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -10,12 +11,21 @@ import java.time.LocalDateTime;
 @Data
 public class chatMessage {
     private long id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss",timezone = "Asia/Seoul")
     private LocalDateTime createDate;
     private String authorName;
     private String content;
 
     public chatMessage(String authorName, String content) {
-        this(1, LocalDateTime.now(), authorName, content);
+        this(ChatMessageIdGenerator.getNextId(), LocalDateTime.now(), authorName, content);
     }
+}
 
+class ChatMessageIdGenerator {
+
+    private static long id = 0;
+
+    public static long getNextId() {
+        return ++id;
+    }
 }
